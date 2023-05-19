7e09932fc94887da8a072ca48b0b04b843659df1
solartejoseluis <solartejoseluis@gmail.com>
2023-05-19 11:03:42 -0500

    adecuando el modulo login y paginas principales
    se adecua modulo login
    se crean paginas principales
    se crea base de datos
    se realiza backup de base de datos
    se crea seccion de bitacora para llevarla en el mismo proyecto

--- app/01_login/login_mdl.php
+++ app/01_login/login_mdl.php
@@ -6,7 +6,7 @@ $user_local = $_POST['npt_user'];
 $password_local = $_POST['npt_password'];
 
 // CONECTAR BASE DE DATOS
-$conn = @mysqli_connect("localhost", "kiron", "123456","CONTROLCAJA");
+$conn = @mysqli_connect("localhost", "kiron", "123456","GESTIONEDUCATIVA");
 
 if (!$conn){
   echo "Fallo al conectar la bd";
@@ -19,7 +19,7 @@ if (!$conn){
   };
 // capturar array con resultados
 while ($fila = mysqli_fetch_array($resultado)){
-  $user_bd = $fila['user_user'];
+  $user_bd = $fila['user_usuario'];
   $password_bd = $fila['user_password'];
 
   if($user_local == $user_bd & $password_local == $password_bd){
@@ -32,25 +32,32 @@ while ($fila = mysqli_fetch_array($resultado)){
 // REDIRECCION DE PAGINA
 if ($count=='1'){
   switch ($user_perfil){
-    case '1':
+      case '1': //perfil estudiante
       $_SESSION['user_bd'] = $user_local;
       $_SESSION['password_bd'] = $password_local;
       $_SESSION['user_id'] = $user_id;
-      echo '<meta http-equiv="REFRESH"content="0;url=../03_turnos/turno_opcion_view.html">';
+      echo '<meta http-equiv="REFRESH"content="0;url=../02_estudiante/estud_home_view.html">';
       break;
 
-    case '2':
+    case '2': // perfil docente
       $_SESSION['user_bd'] = $user_local;
       $_SESSION['password_bd'] = $password_local;
       $_SESSION['user perfil'] = $user_perfil;
-      echo'<meta http-equiv="REFRESH"content="0;url=../04_admin/admin_home_view.html">';
+      echo'<meta http-equiv="REFRESH"content="0;url=../03_docente/docen_home_view.html">';
       break;
 
-    case '3':
+    case '3': //perfil coordinador
       $_SESSION['user_bd'] = $user_local;
       $_SESSION['password_bd'] = $password_local;
       $_SESSION['user perfil'] = $user_perfil;
-      echo'<meta http-equiv="REFRESH"content="0;url=../06_system/turno_todos_view.html">';
+      echo'<meta http-equiv="REFRESH"content="0;url=../04_coordinador/coord_home_view.html">';
+      break;
+
+    case '3': //perfil system_admin
+      $_SESSION['user_bd'] = $user_local;
+      $_SESSION['password_bd'] = $password_local;
+      $_SESSION['user perfil'] = $user_perfil;
+      echo'<meta http-equiv="REFRESH"content="0;url=../05_system/system_home_view.html">';
       break;
   }
 }else{
